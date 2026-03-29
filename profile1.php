72388f47faccc8dfb10f076179d0541c90cf17eb
fina <alfidina@student.ub.ac.id>
2026-03-30 02:03:45 +0700

    Add files via upload

---
+++ profile1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil Saya</title>
+</head>
+<body>
+
+<h1>Profil Mahasiswa</h1>
+
+<p>Nama: Alfi Rizqi Rachmadina</p>
+<p>NIM: 253140707111125</p>
+<p>Kelas: T 2 H</p>
+
+</body>
+</html>
